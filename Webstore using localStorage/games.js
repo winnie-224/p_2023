06ceec1fd0426129ec28904c806cbf04bcaf56fa
winnie-224 <61863506+winnie-224@users.noreply.github.com>
2023-04-12 19:24:58 +0530

    Webstore using localStorage

---
+++ Webstore using localStorage/games.js
@@ -0,0 +1,25 @@
+let games = [
+	{
+	"Title":"Mario Kart 1",
+	"Developer":"AP",
+	"Release":"2",
+	"Description":"New Game",
+	"Price":"$1000",
+	"Genre":"Arcade",
+	"Rating":"3",
+	"Platform":["XBOX","PS2"],
+	"image":""
+	},
+	{
+	"Title":"Tekken",
+	"Developer":"DEVQW",
+	"Release":"5",
+	"Description":"New Game",
+	"Price":"$1433",
+	"Genre":"Action",
+	"Rating":"4",
+	"Platform":["XBOX","PS2"],
+	"image":""
+	}
+	
+]
